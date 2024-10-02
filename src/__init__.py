9dd609a42d9d19f58c82e86311385b626d512b7b
Diego Vasque Maldonado <diego.vasque.maldonado@gmail.com>
2024-10-02 15:35:04 -0300

    llm-multimodal

--- src/__init__.py
+++ src/__init__.py
@@ -9,10 +9,16 @@ app = create_app()
 
 
 from .core.llm import ILLM, LLMBuilder
+
 def init_llm():
     illm: ILLM = LLMBuilder(llm_type='ollama', model_name='llama3', temperature=0)
     return illm.get_instance()
 illm = init_llm()
+
+def init_llm_vision():
+    illm: ILLM = LLMBuilder(llm_type='ollama', model_name='llava', temperature=0)
+    return illm.get_instance()
+illm_vision = init_llm_vision()
 
 from .core.agent import AgentBuilder
 def init_agent():
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from . import app, illm, agent
+import base64
+from . import app, illm, agent, illm_vision
 from asgiref.wsgi import WsgiToAsgi                                             # type: ignore
 from flask import render_template, request, jsonify                             # type: ignore
 
@@ -14,10 +15,19 @@ def send_message():
     if request.method == 'POST':
 
         user_message = request.json.get('message')
+        files = request.json.get('files')
 
         if 'run-agent' in user_message:
             response = agent.invoke(user_message)
             return jsonify({'response': response['output']})
+        elif files is not None:
+            response = illm_vision.bind(images=[
+                str(file['content']).replace('data:image/jpeg;base64,', '') 
+                for file in files
+            ]).invoke(
+                user_message if '' not in user_message else 'descreva a imagem'
+            )
+            return jsonify({'response': str(response)})
         else:
             response = illm.invoke(user_message)
             return jsonify({'response': response})
